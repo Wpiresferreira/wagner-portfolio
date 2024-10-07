a5bef5fdeed3a4137b4af0c1c70b30798103101b
Wagner Pires Ferreira <wagner_pires@icloud.com>
2024-10-06 23:09:58 -0600

    Include animation on the side bar. Change icon when sidebar is hidden/showed.

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -9,18 +9,19 @@ export default function Sidebar() {
   }
   return (
     <>
-      <button onClick={toggleMenu}>
-        <span className="fa fa-align-justify py-8 px-4"></span>
-      </button>
-      <div onClick={toggleMenu} className={` ${showMenu? "max-w-[85vw]": "max-w-4"} overflow-hidden flex flex-col w-[80vw] bg-[--color1] text-[--background] `}>
-        <div className="p-4">
-          <span className="fa fa-user px-4"></span>About me
+      <div  className={` ${showMenu? "max-w-[85vw] animate-[show-menu_1s_ease-in-out_1]": "max-w-4 animate-[hide-menu_1s_ease-in-out_1]"} overflow-hidden flex flex-col w-[80vw] bg-[--color1] text-[--background] 
+         `}>
+           <button onClick={toggleMenu}>
+             <span className={` ${showMenu? "fa-close" : "fa-bars"} w-[85vw] mr-4 fa fa-lg py-8 px-4 `}></span>
+           </button>
+        <div className="p-5 whitespace-nowrap text-2xl">
+          <span className="fa fa-user px-6 fa-lg"></span>About me
         </div>
-        <div className="p-4">
-          <span className="fa fa-user px-4"></span>Some Projects
+        <div className="p-5 whitespace-nowrap text-2xl">
+          <span className="fa  fa-bullseye  fa-lg px-6"></span>Some Projects
         </div>
-        <div className="p-4">
-          <span className="fa fa-user px-4"></span>Resume
+        <div className="p-5 whitespace-nowrap text-2xl">
+          <span className="fa fa-file-text-o fa-lg px-6"></span>Resume
         </div>
         {/* <div className="relative inline-block bg-white fa rounded-r-2xl fa-chevron-right h-[100%]" */}
       </div>
